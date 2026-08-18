f0f6cd019e3fe836ad52871f291770e67cb89da7
RetroFrost <routerifter@gmail.com>
2026-08-18 14:13:56 -0300

    Add Android encoder Gradle library

---
+++ crates/watchcompare-android-encoder/android/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "network.creative.watchcompare.encoder"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":tauri-android"))
+}
